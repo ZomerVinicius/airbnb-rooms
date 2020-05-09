951d07a46fffb211f29df4fdd4f47efb73cd68b7
zomervinicius <vinicius.zomer123@gmail.com>
2020-05-09 15:28:08 -0300

    add airbnb rooms

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const airbnbRoomsUrl =
+  "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72"
+
+async function getAirbnbRooms() {
+  const result = fetch(airbnbRoomsUrl)
+    .then(async (r) => await r.json())
+    .catch((e) => "We couldn't get the airbnb rooms")
+
+  setAirbnbRoomsInHtml(await result)
+}
+
+function setAirbnbRoomsInHtml(airbnbRooms) {
+  const airbnbRoomsDiv = document.getElementById("airbnb-rooms")
+
+  const airbnbCards = `
+    <div class="row">
+    ${airbnbRooms
+      .map(
+        (item) => `
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+            <img src=${item.photo} width="600" height="500" />
+            <div class="card-body">
+                <p class="card-text">${item.name}</p>
+                <div class="d-flex justify-content-between align-items-center float">
+                <h5>${item.property_type}</h5>
+                <div class="badge badge-primary text-wrap ml-auto">
+                    <h5 class="mb-0">R$${item.price}/dia</h5>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        `
+      )
+      .join("")}
+    </div>
+  `
+  airbnbRoomsDiv.innerHTML = airbnbCards
+}
